9f8b2a9ba9a120fee8406948d7a2adae8d28f19b
skuzzy <skuzzytopten@gmail.com>
2025-09-20 23:58:19 +0700

    database connection

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "1234";
+    $database = "php_tutorial";
+    $conn = mysqli_connect($host, $user, $password, $database);
+    if(!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    } else {
+        echo "Database connected successfully";
+    }
+    
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Form</title>
+</head>
+<body>
+    <main>
+        <?php
+            include('connection.php');
+        ?>
+    </main>
+</body>
+</html>
